9fe310039f3763713d73613202fc045c7553ee12
AnsonTu <anson9137@gmail.com>
2020-11-21 16:09:25 -0500

    Applied prettier across files

--- client/src/components/PageContainer/index.js
+++ client/src/components/PageContainer/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import Grid from "@material-ui/core/Grid";
-import {makeStyles} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyle = makeStyles((theme) =>({
+const useStyle = makeStyles((theme) => ({
   rootContainer: {
     background: "#cfe8fc",
     minHeight: "100vh",
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import PageContainer from '../../components/PageContainer'
+import React from "react";
+import PageContainer from "../../components/PageContainer";
+
 function Home() {
-
-    return(
-        <PageContainer> 
-          <h2>Home</h2>
+  return (
+    <PageContainer>
+      <h2>Home</h2>
     </PageContainer>
-        )}
+  );
+}
 
-  export default Home
+export default Home;
