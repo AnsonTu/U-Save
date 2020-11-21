f851e38babfd0f8b005a65adf7d013bef0930c33
AnsonTu <anson9137@gmail.com>
2020-11-21 16:22:06 -0500

    Moved navbar component to components folder

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import HomeIcon from '@material-ui/icons/Home';
-import StorefrontIcon from '@material-ui/icons/Storefront';
-import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
-import PeopleIcon from '@material-ui/icons/People';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import React, { useState } from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import HomeIcon from "@material-ui/icons/Home";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import PeopleIcon from "@material-ui/icons/People";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
+import LocalShippingIcon from "@material-ui/icons/LocalShipping";
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1,
-    
-
-  },
+    flexGrow: 1
+  }
 });
 
 function Navbar() {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
