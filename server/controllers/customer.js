3c893eabd1e6414b8d910db58cf51c538f00c15c
AnsonTu <anson9137@gmail.com>
2020-11-22 21:08:24 -0500

    Added new routes and endpoints

--- server/controllers/customer.js
+++ server/controllers/customer.js
@@ -2,7 +2,7 @@ const pool = require("./pool");
 
 // Get all customers in the database
 exports.getCustomers = (req, res) => {
-  pool.query(`SELECT * FROM customer`, (err, results) => {
+  pool.query(`SELECT * FROM customer;`, (err, results) => {
     if (err) {
       throw err;
     }
@@ -16,13 +16,79 @@ exports.getCustomer = (req, res) => {
   const id = parseInt(req.params.id);
 
   pool.query(
-    `SELECT * FROM customer WHERE customer_id=${id}`,
+    `SELECT * FROM customer WHERE customer_id=${id};`,
     (err, results) => {
       if (err) {
         throw err;
       }
 
       res.status(200).json(results.rows);
+    }
+  );
+};
+
+// View 5: Get all addresses from customers and suppliers for logistical purposes
+exports.getGeographicInfo = (req, res) => {
+  pool.query(
+    `SELECT address FROM customer
+    UNION
+    SELECT address FROM supplier;`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+// View 8: Get the name, phone number, and email of all customers who have an active order
+exports.getActiveCustomersInfo = (req, res) => {
+  pool.query(
+    `SELECT name, phone, email FROM customer
+    JOIN order_details ON customer.customer_id=order_details.customer_id;`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+// View 9: Get the name, phone number, and email of all customers who ordered something in October
+exports.getOctoberCustomers = (req, res) => {
+  pool.query(
+    `SELECT DISTINCT name, phone, email FROM customer
+    JOIN order_details ON customer.customer_id=order_details.customer_id
+    WHERE order_details.order_date >= '2020-10-01' AND order_details.order_date <= '2020-10-31';`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+// Add a new customer
+exports.addCustomer = (req, res) => {
+  const { name, phone, email, address } = req.body;
+
+  pool.query(
+    `INSERT INTO customer(name, phone, email, address) 
+    VALUES('${name}', '${phone}', '${email}', '${address}')
+    RETURNING name, phone, email, address;`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      // Return the newly created customer
+      res.status(201).send(results.rows);
     }
   );
 };
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,1 +1,78 @@
 const pool = require("./pool");
+
+// Get all products in the database
+exports.getProducts = (req, res) => {
+  pool.query(`SELECT * FROM product;`, (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    res.status(200).json(results.rows);
+  });
+};
+
+// Get a product's info based on the given id
+exports.getProduct = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  pool.query(
+    `SELECT * FROM product WHERE product_id=${id};`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+// View 3: Get all ingredients where their price is less than the average price of all existing products
+exports.getBelowAveragePrices = (req, res) => {
+  pool.query(
+    `SELECT name, price FROM product 
+    WHERE price < (SELECT AVG(price) FROM product);`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+// View 6: Get the names of all products that belong to a specific supplier
+exports.getSupplierProducts = (req, res) => {
+  const supplier_id = parseInt(req.params.id);
+
+  pool.query(
+    `SELECT DISTINCT product.name FROM product
+    JOIN supplier ON product.supplier_id=${supplier_id}`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+// Add a new product
+exports.addProduct = (req, res) => {
+  const { name, quantity, price, supplier_id } = req.body;
+
+  pool.query(
+    `INSERT INTO product(name, quantity, price, supplier_id) 
+    VALUES('${name}', '${quantity}', ${price}, ${supplier_id})`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      // Return the added product
+      res.status(201).json(req.body);
+    }
+  );
+};
