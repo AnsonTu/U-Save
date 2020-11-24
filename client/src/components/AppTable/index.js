bc2fe282b4e728fa2b931148be35458fd61d1936
AnsonTu <anson9137@gmail.com>
2020-11-23 20:19:05 -0500

    Add some margin under AppTable

--- client/src/components/AppTable/index.js
+++ client/src/components/AppTable/index.js
@@ -10,7 +10,7 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles({
-  tableContainer: { maxHeight: 650, maxWidth: 1200 },
+  tableContainer: { maxHeight: 650, maxWidth: 1200, marginBottom: "10px" },
   table: {
     minWidth: 700
   },
