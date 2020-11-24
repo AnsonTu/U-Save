0bc758f83ee4b87536f0491ab71d3109e276e693
Kaushik <kaushik.ramani@uoit.net>
2020-11-24 17:58:27 -0500

    Added homecontent

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import PageContainer from "../../components/PageContainer";
-
+import GoalsCard from "../../components/Cards/GoalsCard";
+import NameCard from "../../components/Cards/NameCard";
+import MembersCard from "../../components/Cards/MembersCard";
+import TechCard from "../../components/Cards/TechCard";
 function Home() {
   return (
     <PageContainer>
       <h2>Home</h2>
+      <NameCard />
+      <br />
+      <MembersCard />
+      <br />
+      <GoalsCard />
+      <br />
+      <TechCard />
     </PageContainer>
   );
 }
